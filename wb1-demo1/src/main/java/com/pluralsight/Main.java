ca1c891a4f807d65b829bf57fc1176576e71e5f7
mattchristenson <matt@rcc.team>
2025-04-14 10:52:11 -0500

    finish first lecture

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -15,6 +15,36 @@ public class Main {
 
         System.out.println(greeting);
 
+        ///////////////////////////////////////////////
+
+        float taxRate = 0.15f;
+
+        long aLargeNumber = 2222222L;
+
+
+        ///////////////////////
+
+        int aint;
+
+        long blong;
+
+        aint = 7;
+
+        blong = aint;
+
+////////////////////
+
+        long aLong;
+        int bInt;
+
+        aLong = 7777777777_000_000_070L;
+        bInt = (int) aLong;
+
+
+
+
+        System.out.println(bInt);
+
     }
 
 }
