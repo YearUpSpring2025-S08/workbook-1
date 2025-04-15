343d55c064782300eec840781a6e3bacf81b673f
mattchristenson <matt@rcc.team>
2025-04-15 07:45:20 -0500

    multiple projects file structure set up

--- MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -82,6 +82,11 @@ public class MathApp {
 
         ///////////////////////////////////////////////
 
+        int id = 10135;
+        String name = "Brandon Plyers";
+        float pay = 5239.77f;
+        System.out.printf("%s (id: %d) $%.2f", name, id, pay);
+
     }
 
 }
